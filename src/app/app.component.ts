6d8ddfcc24aa6b2a50f72ee52821d168845db39c
LL\bhapatha1 <bhavik.pathak@publicissapient.com>
2023-04-28 18:29:35 +0530

    Added a notification for screen size

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
-import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { InitService } from './core/services/init.service';
+import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmDialogComponent } from './shared/component/confirm-dialog/confirm-dialog.component';
 
 @Component({
   selector: 'app-root',
@@ -8,8 +9,29 @@ import { InitService } from './core/services/init.service';
   styleUrls: ['./app.component.scss'],
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  constructor() {}
+  constructor(public breakpointObserver: BreakpointObserver,private dialog: MatDialog) {}
+
+  ngOnInit():void{
+    this.breakpointObserver
+      .observe([Breakpoints.XSmall,Breakpoints.Small,Breakpoints.TabletPortrait])
+      .subscribe((state: BreakpointState) => {
+        if (state.matches) {
+          let dialogObj = {
+            maxWidth:390,
+            disableClose: false,
+            data: {
+              okButtonText: 'Continue',
+              cancelButtonText: 'No',
+              hideCancel: 'yes',
+              title: 'Optimal Viewing Experience',
+              message: `Please rotate to landscape mode for optimal viewing. Screen optimized for medium-sized devices. Mobile optimization in progress. Thank you.`,
+            }
+          }
+          this.dialog?.open(ConfirmDialogComponent, dialogObj);
+        }
+      });
+  }
 
 }
